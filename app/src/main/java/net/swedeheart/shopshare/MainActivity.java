3e1522d1153087cd47941c28ede7d6e55a1abb79
Phil Bambridge <bvaspala@users.noreply.github.com>
2020-10-14 23:25:18 +0100

    Inital UI and MVP functionality

--- app/src/main/java/net/swedeheart/shopshare/MainActivity.java
+++ app/src/main/java/net/swedeheart/shopshare/MainActivity.java
@@ -3,6 +3,13 @@ package net.swedeheart.shopshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleNumber(View view) {
+        //TODO Prevent leading zero, more than one decimal point, more than two decimal places
+        TextView ic = findViewById(R.id.itemCost);
+        String currentTotal = ic.getText().toString();
+        ic.setText((currentTotal.equals("0.00") ? "" : currentTotal) + ((Button) view).getText().toString());
+    }
+
+    public void addFull(View view) {
+        add(false);
+    }
+
+    public void addHalf(View view) {
+        add(true);
+    }
+
+    private void add(boolean half) {
+        TextView rt = findViewById(R.id.runningTotal);
+        TextView ic = findViewById(R.id.itemCost);
+        BigDecimal rtNum = new BigDecimal(rt.getText().toString());
+        BigDecimal icNum = new BigDecimal(ic.getText().toString());
+        rtNum = rtNum.add(half ? icNum.divide(new BigDecimal(2)) : icNum);
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        nf.setMinimumFractionDigits(2);
+        rt.setText(nf.format(rtNum));
+        ic.setText("0.00");
+    }
+
+    public void deleteChar(View view) {
+        TextView ic = findViewById(R.id.itemCost);
+        CharSequence icText = ic.getText();
+        if (ic.length() > 0) {
+            ic.setText(icText.subSequence(0, icText.length() - 1));
+        }
     }
 }
